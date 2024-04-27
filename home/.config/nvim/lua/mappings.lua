fa5b20c8f4d76db584c5ce495fc158675af8ef7e
Andy Potter <andy@andypotter.co.uk>
2024-04-27 10:44:16 +0100

    Initial commit

---
+++ home/.config/nvim/lua/mappings.lua
@@ -0,0 +1,5 @@
+vim.keymap.set("i", "jj", "<ESC>", nil)
+vim.keymap.set("n", "<C-d>", "<C-d>zz", nil)
+vim.keymap.set("n", "<C-u>", "<C-u>zz", nil)
+vim.keymap.set("n", "n", "nzz", nil)
+vim.keymap.set("n", "N", "Nzz", nil)
---
+++ home/.config/nvim/lua/options.lua
@@ -0,0 +1,20 @@
+vim.opt.expandtab = true
+vim.opt.smarttab = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+
+vim.opt.mouse = "a"
+vim.opt.clipboard = "unnamedplus"
+
+vim.opt.scrolloff = 5
+vim.opt.hidden = true -- allow buffers to remain open in background with unsaved changes
+vim.opt.number = true -- enable line numbers
+vim.opt.signcolumn = "number" -- show diag errors in the number column (saves on width)
+vim.opt.relativenumber = true
+vim.opt.splitright = true
+
+vim.g.c_syntax_for_h = true
